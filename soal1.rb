a3314d92a96310e2f2e8e374da07072bd66d7391
Dimas Roger <dimasroger89@gmail.com>
2020-02-19 14:13:35 +0700

    bismillah

--- soal1.rb
+++ soal1.rb
@@ -2,17 +2,47 @@
 # Contoh
 # 63 = 1:03 (Satu menit tiga detik)
 
+<<<<<<< HEAD
 def konversiMenit(menit) 
+=======
+def konversiMenit(menit)
+>>>>>>> bismillah
   # =================================
   # Area Kode Kode di sini
+    # puts menit
+  
 
+    if menit >= 60
+
+        cek = menit / 60
+
+        mod = menit % 60
+        if mod < 10
+          mod = "0#{mod}"
+        end
+        puts "#{cek}:#{mod}"
+        
+    else
+      
+      cek = 0
+      mod = menit
+      puts "#{cek}:#{mod}"
+
+    end
+
+    
 
 
   # =================================
 end
+<<<<<<< HEAD
+=======
+
+>>>>>>> bismillah
 
 # Hasilnya harus seperti di bawah. contoh: 0:32
 konversiMenit(32) # 0:32
 konversiMenit(63) # 1:03
 konversiMenit(133) # 2:13
 konversiMenit(200) # 3:20
+
