17ee8b90880520a0badac71099b26907c85a24b8
cecenaila <fviantynaila@gmail.com>
2020-02-19 14:21:31 +0700

    yeaaa

--- soal1.rb
+++ soal1.rb
@@ -2,14 +2,25 @@
 # Contoh
 # 63 = 1:03 (Satu menit tiga detik)
 
+<<<<<<< HEAD
 def konversiMenit(menit) 
+=======
+def konversiMenit(menit)
+>>>>>>> yeaaa
   # =================================
   # Area Kode Kode di sini
+  detik = menit % 60
+  menit = menit  / 60
 
-
+  detik = " 0 #{detik}" if detik < 10
+  puts "#{menit} : #{detik}"
 
   # =================================
+<<<<<<< HEAD
 end
+=======
+  end
+>>>>>>> yeaaa
 
 # Hasilnya harus seperti di bawah. contoh: 0:32
 konversiMenit(32) # 0:32
