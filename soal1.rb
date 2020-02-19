f2426a5c7e9794becf5a7a92e904d0efb63e366a
mariadelight <mariadeliguori9@gmail.com>
2020-02-19 14:25:20 +0700

    halo

--- soal1.rb
+++ soal1.rb
@@ -6,10 +6,19 @@ def konversiMenit(menit)
   # =================================
   # Area Kode Kode di sini
 
+  detik = menit % 60
+  menit = menit / 60
+
+  detik = " 0 #{detik}" if detik < 10
+  puts "#{menit} : #{detik}"
 
 
   # =================================
 end
+<<<<<<< HEAD
+=======
+
+>>>>>>> halo
 
 # Hasilnya harus seperti di bawah. contoh: 0:32
 konversiMenit(32) # 0:32
