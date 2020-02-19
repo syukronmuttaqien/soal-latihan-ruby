680612d847bc9d6738406404d6573d57074dea0c
muhamadzulkifli <zul397@gmail.com>
2020-02-19 14:01:30 +0700

    zulkifli

--- soal1.rb
+++ soal1.rb
@@ -2,15 +2,19 @@
 # Contoh
 # 63 = 1:03 (Satu menit tiga detik)
 
-def konversiMenit(menit) {
+def konversiMenit(menit) 
   # =================================
   # Area Kode Kode di sini
-
-
+  if (menit%60<10)
+    puts "hasil konversi #{menit/60}:0#{menit%60}"
+  else
+    puts "hasil konversi #{menit/60}:#{menit%60}"
+  end
+  
 
   # =================================
-}
 
+end
 # Hasilnya harus seperti di bawah. contoh: 0:32
 konversiMenit(32) # 0:32
 konversiMenit(63) # 1:03
