d291bd62a3a43ac6e66e56c2e82af28db24fd4e6
Syukron Muttaqien <syukronmuttaqien74@gmail.com>
2020-02-19 13:25:12 +0700

    Menambahkan 1 Soal

---
+++ soal1.rb
@@ -0,0 +1,18 @@
+# Konversi Menit ke Jam
+# Contoh
+# 63 = 1:03 (Satu menit tiga detik)
+
+def konversiMenit(menit) {
+  # =================================
+  # Area Kode Kode di sini
+
+
+
+  # =================================
+}
+
+# Hasilnya harus seperti di bawah. contoh: 0:32
+konversiMenit(32) # 0:32
+konversiMenit(63) # 1:03
+konversiMenit(133) # 2:13
+konversiMenit(200) # 3:20
