fd3822a77aea5cbb024327ef2d4975fdf432722b
AthaBeryl <berylprimapes@gmail.com>
2020-02-19 13:58:40 +0700

    selesai,perpendek sintak

--- soal1.rb
+++ soal1.rb
@@ -5,16 +5,13 @@
 def konversiMenit(menit) 
   # =================================
   # Area Kode Kode di sini
-  if(menit%60<10)
-  puts"Hasil Konversi #{menit/60}:0#{menit%60}"
-  else
-  puts"Hasil Konversi #{menit/60}:#{menit%60}"
-  end
+  puts"Hasil Konversi #{menit/60}:0#{menit%60}"if(menit%60<10)
+  puts"Hasil Konversi #{menit/60}:#{menit%60}"if(menit%60>=10)
   # =================================
 end
 
 # Hasilnya harus seperti di bawah. contoh: 0:32
 konversiMenit(32) # 0:32
-konversiMenit(63) # 1:03
+konversiMenit(70) # 1:03
 konversiMenit(133) # 2:13
 konversiMenit(200) # 3:20
